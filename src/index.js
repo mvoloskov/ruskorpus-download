95bf5f06d6ae51b02632bd6e9a158d14a0da6e94
mvoloskov <hello@miloslav.website>
2021-04-24 00:28:39 +0300

    export done

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import download from './download'
+import htmlify from './htmlify'
 
 const start = () => {
-  
-  const list = Array.from(document.querySelectorAll('.pager ~ ol > li'))
-  const word = document.querySelector('p.res:nth-child(2n)').textContent.trim().replaceAll(`"`, ``)
-  const results = list.map(node => {
-    const heading = node.querySelector('span.b-doc-expl').textContent.trim()
-    const text = node.querySelector('li').textContent.trim()
-      .replace('\n\n             \n            [омонимия снята]\n            \n\n\n             \n             ←…→', '')
-      .replace('\n\n             \n            [омонимия не снята]\n            \n\n\n             \n             ←…→', '')
-      .replace('\n             ', '')
-      // .replaceAll(word, `<b>${word}</b>`)
-      .replaceAll(word, `[${word}]`)
-    return { heading, text }
-  })
+    
+    const list = Array.from(document.querySelectorAll('.pager ~ ol > li'))
+    const pageNumber = document.querySelector('.pager > b').textContent.trim()
+    const word = document.querySelector('p.res:nth-child(2n)').textContent.trim().replaceAll(`"`, ``)
+    const results = list.map(node => {
+        const heading = node.querySelector('span.b-doc-expl').textContent.trim()
+        const text = node.querySelector('li').textContent
+            .replace('[омонимия снята]', '')
+            .replace('[омонимия не снята]', '')
+            .replace('←…→', '')
+            .trim()
+            .replaceAll(word, `<b>${word}</b>`)
+        return { heading, text }
+    })
 
-  // console.log(word, results[0])
+    const resultsInnerHtml = results.map(result => `
+        <li>
+            <p><b>${result.heading}</b></p>
+            <p>${result.text}</p>
+        </li>
+    `)
 
-  // const resultsInnerHtml = results.map(result => `
-  //   <li>
-  //     <p><b>${result.heading}</b></p>
-  //     <p>${result.text}</p>
-  //   </li>
-  // `)
-
-  // const resultsHtml = `
-  //   <ol>
-  //     ${resultsInnerHtml.join('\n')}
-  //   </ol>
-  // `
-
-  const resultsText = results.map(result => `${result.heading}\n\t${result.text}\n`).join('\n')
-  download(resultsText, 'text/txt', 'results.txt')
+    const resultsHtml = `
+        <ol>
+            ${resultsInnerHtml.join('\n')}
+        </ol>
+    `
+    const filename = `${word} (стр. ${pageNumber}).html`
+    const resultsHTML = htmlify(filename, resultsHtml)
+    download(resultsHTML, 'text/html', filename)
 }
 
 if (document.readyState === 'interactive' || document.readyState === 'complete') {
-  start()
+    start()
 } else {
-  document.addEventListener('DOMContentLoaded', start)
+    document.addEventListener('DOMContentLoaded', start)
 }
